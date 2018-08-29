8115142664ab809639d918f84a5be56946df949d
anlgncr <anlgncr@gmail.com>
2018-08-29 20:54:55 +0300

    Update DS3231.cpp

--- DS3231/DS3231.cpp
+++ DS3231/DS3231.cpp
@@ -8,13 +8,13 @@ DS3231::DS3231(uint8_t device_address){
 bool DS3231::setTime(uint8_t hours, uint8_t minutes, uint8_t seconds){
 	Wire.beginTransmission(my_device_address);
 	Wire.write(0x00);
-    Wire.write(decToBcd(seconds));
-    Wire.write(decToBcd(minutes));
+	Wire.write(decToBcd(seconds));
+	Wire.write(decToBcd(minutes));
 	Wire.write(decToBcd(hours));
 
 	bool result = Wire.endTransmission();
 	
-    if(result == 0) 
+	if(result == 0) 
 		return true;
 	else
 		return false;
@@ -23,14 +23,14 @@ bool DS3231::setTime(uint8_t hours, uint8_t minutes, uint8_t seconds){
 bool DS3231::setDate(uint8_t day, uint8_t date, uint8_t month, uint8_t year){
 	Wire.beginTransmission(my_device_address);
 	Wire.write(0x03);
-    Wire.write(decToBcd(day));
-    Wire.write(decToBcd(date));
+	Wire.write(decToBcd(day));
+	Wire.write(decToBcd(date));
 	Wire.write(decToBcd(month));
 	Wire.write(decToBcd(year));
 
 	bool result = Wire.endTransmission();
 	
-    if(result == 0) 
+	if(result == 0) 
 		return true;
 	else
 		return false;
@@ -38,12 +38,12 @@ bool DS3231::setDate(uint8_t day, uint8_t date, uint8_t month, uint8_t year){
 
 void DS3231::getTime(dateTime* my_dt){
 	Wire.beginTransmission(my_device_address);
-    Wire.write(0x00);
+	Wire.write(0x00);
 	Wire.endTransmission();
-	
+
 	Wire.requestFrom(my_device_address, (uint8_t)3); 
-    if (Wire.available()){
-        my_dt->seconds = bcdToDec(Wire.read());
+	if (Wire.available()){
+		my_dt->seconds = bcdToDec(Wire.read());
 		my_dt->minutes = bcdToDec(Wire.read());
 		my_dt->hours = bcdToDec(Wire.read());
 	}
@@ -51,12 +51,12 @@ void DS3231::getTime(dateTime* my_dt){
 
 void DS3231::getDate(dateTime* my_dt){
 	Wire.beginTransmission(my_device_address);
-    Wire.write(0x03);
+	Wire.write(0x03);
 	Wire.endTransmission();
-	
+
 	Wire.requestFrom(my_device_address, (uint8_t)4); 
-    if (Wire.available()){
-        my_dt->day = bcdToDec(Wire.read());
+	if (Wire.available()){
+		my_dt->day = bcdToDec(Wire.read());
 		my_dt->date = bcdToDec(Wire.read());
 		my_dt->month = bcdToDec(Wire.read());
 		my_dt->year = bcdToDec(Wire.read());
@@ -65,20 +65,20 @@ void DS3231::getDate(dateTime* my_dt){
 
 float DS3231::getTemperature(){
 	Wire.beginTransmission(my_device_address);
-    Wire.write(0x11);
+	Wire.write(0x11);
 	Wire.endTransmission();
-	
+
 	uint8_t upper, lower;
-	
+
 	Wire.requestFrom(my_device_address, (uint8_t)2); 
-    if (Wire.available()){
-        upper = Wire.read();
+	if (Wire.available()){
+		upper = Wire.read();
 		lower = Wire.read();
 	}
-	
+
 	if(upper >> 7) // 8 bit indicates that degree is under 0
 		upper = upper & ~(0x80);
-	
+
 	return upper + (lower >> 6)* 0.25;
 }
 
